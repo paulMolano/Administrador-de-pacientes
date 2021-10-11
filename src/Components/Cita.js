c0db899f8625c9979f305c9c7e654ade06b00921
Paul Molano <paulmolanoo@gmail.com>
2021-10-11 22:18:17 +0200

    Completed

--- src/Components/Cita.js
+++ src/Components/Cita.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const Cita = ({ cita, eliminarCita }) => {
   return (
@@ -29,6 +30,11 @@ const Cita = ({ cita, eliminarCita }) => {
       </div>
     </>
   );
+};
+
+Cita.propTypes = {
+  cita: PropTypes.object.isRequired,
+  eliminarCita: PropTypes.func.isRequired,
 };
 
 export default Cita;
--- src/Components/Formulario.js
+++ src/Components/Formulario.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import PropTypes from "prop-types";
 
 const Formulario = ({ crearCita }) => {
   //Crear State de Citas
@@ -118,6 +119,10 @@ const Formulario = ({ crearCita }) => {
       </form>
     </>
   );
+};
+
+Formulario.propTypes = {
+  crearCita: PropTypes.func.isRequired,
 };
 
 export default Formulario;
